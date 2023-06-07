9a610360801ea4b97b2009ae1ee0aa252c1b5906
Md Somad <mdsomad10@gmail.com>
2023-06-07 10:51:57 +0530

    add

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Tiktok Clone',
       theme:  ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor
       ),
