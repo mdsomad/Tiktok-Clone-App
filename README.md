025c363a924da53790081ae1ba7d87e07ad8cd50
Md Somad <mdsomad10@gmail.com>
2023-06-08 15:48:56 +0530

    home screen ui complete

--- README.md
+++ README.md
@@ -10,19 +10,18 @@ firebase_auth: ^4.6.2
 cloud_firestore: ^4.8.0
 firebase_storage: ^11.2.2 -->
 
+<!--  
+##  Creating User Complete Firebase  -->
 
 
-<!-- ##  Creating User Complete Firebase -->
-
-
-<!-- ## Tiktok Clone App Video Preview & AddCaption_Screen Ui Preview
+## Tiktok Clone App Home Screen Ui Preview
 
 
 <table>
   
   
 <tr>                    
-   <th>Video Preview & Play Ui View</th>
+   <th>Home Screen  Ui View</th>
   
 </tr>  
   
@@ -31,7 +30,7 @@ firebase_storage: ^11.2.2 -->
 <tr>
 
 <td>
-  <img src="https://github.com/mdsomad/Tiktok-Clone-App/assets/103892160/162347c0-44f4-4a42-9bae-6a923cd73195" alt="Video Preview Screen Ui View Example" width="260"/>
+  <img src="" alt="Home Screen Ui View Example" width="260"/>
 </td>
 
   
@@ -39,4 +38,4 @@ firebase_storage: ^11.2.2 -->
 
 
 </table>
- -->
+
--- lib/view/widgets/AlbumRotator.dart
+++ lib/view/widgets/AlbumRotator.dart
@@ -6,12 +6,75 @@ import 'package:flutter/material.dart';
 
 
 
-class AlbumRotator extends StatelessWidget {
+class AlbumRotator extends StatefulWidget {
    String profilePicUrl;
    AlbumRotator({super.key,required this.profilePicUrl});
 
   @override
+  State<AlbumRotator> createState() => _AlbumRotatorState();
+}
+
+class _AlbumRotatorState extends State<AlbumRotator> with SingleTickerProviderStateMixin {
+
+  late AnimationController controller;
+
+ @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = AnimationController(vsync: this,duration: Duration(seconds: 5));
+    controller.forward();
+    controller.repeat();
+
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller.dispose();
+  }
+  
+  
+  
+  
+  
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return RotationTransition(
+      turns: Tween(begin: 0.0,end:1.0).animate(controller),
+      child: SizedBox(
+        height: 70,
+        width: 70,
+        child: Column(
+          children: [
+            Container(
+               height: 60,           
+               width: 60,
+               decoration: BoxDecoration(
+                  gradient: LinearGradient(colors:[
+                    Colors.green,
+                    Colors.white,
+                    ]),
+                    borderRadius: BorderRadius.circular(35),
+                ),
+                
+                
+                child:ClipRRect(
+                  borderRadius: BorderRadius.circular(35),
+                  child: 
+                  Image(
+                    image: 
+                    //  NetworkImage(profilePicUrl),
+                     NetworkImage('https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'),
+                     fit: BoxFit.cover,
+                     ),
+                  ),
+              ),
+                
+          ],
+        ),
+      ),
+    );
   }
 }
